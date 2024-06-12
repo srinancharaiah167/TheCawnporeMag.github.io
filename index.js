a8af26f0e55caff0c7fa4ed6fbd0152952915290
Kritika Singh <kritikasingh6881@gmail.com>
2024-06-12 11:56:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.right = "0";
+
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
+
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () =>  {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
+
