2c68c2b4673e52dec5e08e8d9d04732888382417
Macha Varshitha Reddy <132129385+Varshitha713@users.noreply.github.com>
2025-07-23 15:08:27 +0530

    added scroll-progress-bar (#56)

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+window.onscroll = function() {
+  scrollProgressBar();
+};
+
+function scrollProgressBar() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("progressBar").style.width = scrolled + "%";
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     var navLinks = document.getElementById("navLinks");
 
