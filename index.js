6e74001664090db66d89442a683b5f422d5cee9d
srinancharaiah167 <nancharaiah41@gmail.com>
2025-08-06 21:29:19 +0530

    Fix: reduce scroll lag

--- index.js
+++ index.js
@@ -61,8 +61,8 @@ document.addEventListener("DOMContentLoaded", function () {
     // Loader
 const loaderScreen = document.getElementById('loading-screen');
 if (loaderScreen) {
-    setTimeout(() => loaderScreen.classList.add('fading'), 1700);
-    setTimeout(() => loaderScreen.classList.add('hidden'), 2000);
+    setTimeout(() => loaderScreen.classList.add('fading'), 300);
+    setTimeout(() => loaderScreen.classList.add('hidden'), 500);
 }
 
 
